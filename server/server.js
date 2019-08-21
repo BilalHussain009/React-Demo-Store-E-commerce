28145601393362e09f4d1371d92f2094e9b7f8b9
BilalHussain009 <bilalhussain.work@gmail.com>
2019-08-21 12:37:11 +0500

    Update

--- server/server.js
+++ server/server.js
@@ -5,16 +5,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 const compression = require('compression');
 app.use(express.static(publicPath));
-app.use(compression({ filter: shouldCompress }));
-function shouldCompress (req, res) {
-  if (req.headers['x-no-compression']) {
-    // don't compress responses with this request header
-    return false
-  }
-
-  // fallback to standard filter function
-  return compression.filter(req, res)
-}
+zz
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
