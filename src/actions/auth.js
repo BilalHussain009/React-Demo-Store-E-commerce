7d28aeec540669aa41547340d6e9e8812b922134
BilalHussain009 <bilalhussain.work@gmail.com>
2019-10-29 22:30:27 +0500

    Go back

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,5 @@
 import { firebase, googleAuthProvider,facebookAuthProvider } from '../firebase/firebase';
-import { history } from '../routers/AppRouter';
+
 export const login = () => ({
   type: 'LOGIN'
   
@@ -9,14 +9,14 @@ const redirectUser=()=>{
 }
 export const startLogin = () => {
   return (dispatch) => {
-    firebase.auth().signInWithPopup(googleAuthProvider).then(()=>{dispatch(login());history.push('/userprofile')
+    firebase.auth().signInWithPopup(googleAuthProvider).then((props)=>{dispatch(login());redirectUser()
       });
   };
 };
 export const startFaceBookLogin=()=>{
   return ()=>{
     
-    firebase.auth().signInWithPopup(facebookAuthProvider).then((props)=>{dispatch(login());redirectUser()});
+    firebase.auth().signInWithPopup(facebookAuthProvider)
   }
 }
 export const logout = () => ({
@@ -25,7 +25,12 @@ export const logout = () => ({
 
 export const startLogout = () => {
   return (dispatch) => {
+      
      firebase.auth().signOut().then(()=>dispatch(logout()));
+     
+     
+
+    
      return;
 
   };
