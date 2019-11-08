50129e97eed33b5931eb340ad683387ac22c89b6
BilalHussain009 <bilalhussain.work@gmail.com>
2019-11-08 22:57:13 +0500

    Webpack Compression

--- server/server.js
+++ server/server.js
@@ -5,9 +5,15 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 const compression = require('compression');
 app.use(express.static(publicPath));
-
+function shouldCompress(req, res) {
+  if (req.headers["x-no-compression"]) return false;
+  return compression.filter(req, res);
+}
 app.get('*', (req, res) => {
-  app.use(compression());
+  app.use(compression({
+    level: 2,              
+    filter: shouldCompress,
+  }));
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
