555978e5d07931b5635328e4706e8c91c2bb3b26
BilalHussain009 <bilalhussain.work@gmail.com>
2019-11-09 23:54:17 +0500

    hello world

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 const compression = require('compression');
-app.use(express.static(publicPath));
+
+app.use(app.use(compression(express.static(publicPath)))
+);
 
 app.get('*', (req, res) => {
-  app.use(compression());
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
