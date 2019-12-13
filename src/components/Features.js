2a4a20c8ece75846881c32998ecfb62ba5840d78
BilalHussain009 <bilalhussain.work@gmail.com>
2019-12-13 23:21:21 +0500

    test for new domain

--- src/components/Features.js
+++ src/components/Features.js
@@ -9,8 +9,7 @@ const Features = () => (
       <h1>Address</h1>
       <p className="maldini-paragraph"> Shop No 17,Block 12,Jinnah Super Market F7, Islamabad, Pakistan-44000 Contact +92-51-2723006
 </p>
-      <h1>General Information</h1>
-      <p className="maldini-paragraph">Maldini makes in every new collection the combination of Grace, Class, Trend and Comfort to the customer. The effort for a good job as well as for the quality of the product makes the difference.
+      <h1>General Information</h1><p className="maldini-paragraph">Maldini makes in every new collection the combination of Grace, Class, Trend and Comfort to the customer. The effort for a good job as well as for the quality of the product makes the difference.
 The styles are addressed to the practical and modern generation.
 Maldini The Italian Brand/Maldini makes in every new collection the combination of Grace, Class, Trend and Comfort to the customer.
 Maldini Fashion
